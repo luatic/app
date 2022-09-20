b4f500de8437250c8cf2444c5f56a41c3726de41
Patrycjusz Myszka <pat.myszka@gmail.com>
2022-09-20 19:38:03 +0100

    style css

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,24 @@
 import React, {Component} from 'react'
 
+let styles = {
+  header: {
+    background: '#03a9fa',
+  },
+  logo: {
+    color: '#fff',
+    fontFamily: 'Anton',
+    textAlign: 'center',
+  },
+}
+
 export default class Header extends Component {
   render() {
     return (
       <>
-        <div>Logo</div>
-        <input />
+        <div style={styles.header}>
+          <div style={styles.logo}>Logo</div>
+          <input />
+        </div>
       </>
     )
   }
