0f4dff0318646be48a3f77dcfaa9d896010abeea
Patrycjusz Myszka <pat.myszka@gmail.com>
2022-09-20 07:46:26 +0100

    testing func

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,14 @@
 import React from 'react'
 
+const getDate = () => {
+  const newDate = new Date()
+  return newDate.getFullYear()
+}
+
 const Header = () => (
   <>
-    <div>the date is : {Date.now()}</div>
+    <div>the date is : {getDate()}</div>
   </>
 )
+
 export default Header
