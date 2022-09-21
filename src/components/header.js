930872cc18541de000ef0c94bb3e24fbc825a5ab
Patrycjusz Myszka <pat.myszka@gmail.com>
2022-09-21 21:19:22 +0100

    some events added

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,17 @@
 import React, {Component} from 'react'
 
 export default class Header extends Component {
+  inputChange = (event) => {
+    console.log(event)
+  }
   render() {
     return (
       <>
         <header>
-          <div className="logo">Logo</div>
-          <input />
+          <div className="logo" onClick={() => console.log('i was clicked')}>
+            Logo
+          </div>
+          <input onChange={this.inputChange} />
         </header>
       </>
     )
